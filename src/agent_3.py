9c4e79ff18c546bf0c2a74801f510f36ed0e0c7d
Naveenan Yogeswaran <naveenan.r.y@gmail.com>
2021-10-11 23:08:05 -0400

    Added classes for agent 2 and 3 and the cells. Agent 3 needs to be completed. Need to figure out how the cells are represented by agent 3.

---
+++ src/agent_3.py
@@ -0,0 +1,18 @@
+from gridworld import Gridworld
+
+class Agent_3:
+
+  def __init__(self, dim):
+    self.dim = dim
+    self.discovered_grid = Gridworld(dim)
+
+  def execute_path(self, path, complete_grid):
+    for index, node in enumerate(path):
+      curr = node.curr_block
+      # check if path is blocked
+      if complete_grid.gridworld[curr[0]][curr[1]] == 1:
+        # update our knowledge of blocked nodes
+        self.discovered_grid.update_grid_obstacle(curr, 1)
+        return node.parent_block
+      self.discovered_grid.update_grid_obstacle(curr, 0)
+    return path[-1]
---
+++ src/cell.py
@@ -0,0 +1,22 @@
+class Cell:
+
+  def __init__(self, x, y, dim):
+    if x == 0 and y == 0:
+      self.neighbors = 3
+    elif x == 0 and y == dim-1:
+      self.neighbors = 3
+    elif x == dim-1 and y == 0:
+      self.neighbors = 3
+    elif x == dim-1 and y == dim-1:
+      self.neighbors = 3
+    elif x == 0 or x == dim-1 or y == 0 or y == dim-1:
+      self.neighbors = 5
+    else:
+      self.neighbors = 8
+      
+    self.visited = False
+    self.confirmed = False
+    self.block_sense = 0
+    self.confirm_block = 0
+    self.confirm_empty = 0
+    self.hidden = 0
